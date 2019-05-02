e5cabd36e171408158dcce43fc5a25d0142016f5
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-05-02 15:37:26 +0300

    perf: directly import component from react-bootstrap
    - react-bootstrap dist contains only cjs exports, so importing from index, results in including the entire lib
    - reduced bundle size from 416KB  down to 14KB

--- src/components/Badge/Badge.jsx
+++ src/components/Badge/Badge.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Badge as BootstrapBadge } from "react-bootstrap";
+import BootstrapBadge from "react-bootstrap/Badge";
 
 import "./Badge.scss";
 
