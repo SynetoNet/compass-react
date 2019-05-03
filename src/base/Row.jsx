0cf013492d4a844449c4408aa71e35ad68b6b7b3
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-05-03 17:29:27 +0300

    feat(Base): Re-export Grid components

---
+++ src/base/Row.jsx
@@ -0,0 +1,1 @@
+export { default as Row } from "react-bootstrap/Row";
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
 export { default as Badge } from "./components/Badge/Badge.jsx";
+
+export { default as Container } from "./base/Container";
+export { default as Row } from "./base/Row";
+export { default as Col } from "./base/Col";
