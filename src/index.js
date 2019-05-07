cbd8217f008c304583424f55aea1b26bc6dd2e3e
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-05-07 17:40:00 +0300

    build: fix build for Base components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export { default as Badge } from "./components/Badge/Badge.jsx";
 export { default as Tabs } from "./components/Tabs/Tabs.jsx";
 
-export { default as Container } from "./base/Container";
-export { default as Row } from "./base/Row";
-export { default as Col } from "./base/Col";
+export { Container } from "./base/Container.jsx";
+export { Row } from "./base/Row.jsx";
+export { Col } from "./base/Col.jsx";
