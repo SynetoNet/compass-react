8405e1d25ea2d32b74d7ad804c461cd1defbe336
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-05-07 17:39:01 +0300

    test(Tabs): fix tests and update snapshots

--- src/components/Tabs/Tabs.spec.js
+++ src/components/Tabs/Tabs.spec.js
@@ -1,20 +1,19 @@
 import React from "react";
 import Tabs from "./Tabs";
-import Tab from "../Tab/Tab";
 import renderer from "react-test-renderer";
 
 test("renders Horizontal Tabs", () => {
   const component = renderer.create(
     <Tabs>
-      <Tab eventKey="tab1" title="tab1 title">
+      <Tabs.Tab eventKey="tab1" title="tab1 title">
         tab1 content
-      </Tab>
-      <Tab eventKey="tab2" title="tab2 title">
+      </Tabs.Tab>
+      <Tabs.Tab eventKey="tab2" title="tab2 title">
         tab2 content
-      </Tab>
-      <Tab eventKey="tab3" title="tab3 title">
+      </Tabs.Tab>
+      <Tabs.Tab eventKey="tab3" title="tab3 title">
         tab3 content
-      </Tab>
+      </Tabs.Tab>
     </Tabs>
   );
   expect(component.toJSON()).toMatchSnapshot();
@@ -23,15 +22,15 @@ test("renders Horizontal Tabs", () => {
 test("renders Vertical Tabs", () => {
   const component = renderer.create(
     <Tabs vertical>
-      <Tab eventKey="tab1" title="tab1 title">
+      <Tabs.Tab eventKey="tab1" title="tab1 title">
         tab1 content
-      </Tab>
-      <Tab eventKey="tab2" title="tab2 title">
+      </Tabs.Tab>
+      <Tabs.Tab eventKey="tab2" title="tab2 title">
         tab2 content
-      </Tab>
-      <Tab eventKey="tab3" title="tab3 title">
+      </Tabs.Tab>
+      <Tabs.Tab eventKey="tab3" title="tab3 title">
         tab3 content
-      </Tab>
+      </Tabs.Tab>
     </Tabs>
   );
   expect(component.toJSON()).toMatchSnapshot();
