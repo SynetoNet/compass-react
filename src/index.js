963c879d8a01403208e333044ae0580b4e27ad7b
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-05-21 10:12:45 +0300

    Expose DatePicker component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as Badge } from "./components/Badge/Badge.jsx";
 export { default as Tabs } from "./components/Tabs/Tabs.jsx";
+export { default as DatePicker } from "./components/DatePicker/DatePicker.jsx";
 
 export { Container } from "./base/Container.jsx";
 export { Row } from "./base/Row.jsx";
