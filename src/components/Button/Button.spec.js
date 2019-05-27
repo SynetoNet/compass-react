65c3e34e9ca48863d856b216d553558b00f15d57
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-05-27 15:15:07 +0300

    test(Button): add snapshop tests

--- src/components/Button/Button.spec.js
+++ src/components/Button/Button.spec.js
@@ -2,25 +2,75 @@ import React from "react";
 import Button from "./Button";
 import renderer from "react-test-renderer";
 
-test("renders default Button", () => {
-  const component = renderer.create(<Button />);
-  expect(component.toJSON()).toMatchSnapshot();
+describe("Default Button", () => {
+  test("secondary Button", () => {
+    const component = renderer.create(<Button>Secondary</Button>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("primary Button", () => {
+    const component = renderer.create(
+      <Button variant="primary">Primary</Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("tertiary Button", () => {
+    const component = renderer.create(
+      <Button variant="tertiary">Tertiary</Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
 
-test("renders primary danger Button", () => {
-  const component = renderer.create(
-    <Button variant="primary" color="danger">
-      Primary Button
-    </Button>
-  );
-  expect(component.toJSON()).toMatchSnapshot();
+describe("Accept Button", () => {
+  test("secondary success Button", () => {
+    const component = renderer.create(
+      <Button color="success">Secondary</Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("primary success Button", () => {
+    const component = renderer.create(
+      <Button color="success" variant="primary">
+        Primary
+      </Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("tertiary success Button", () => {
+    const component = renderer.create(
+      <Button color="success" variant="tertiary">
+        Primary
+      </Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
 
-test("renders disabled Button", () => {
-  const component = renderer.create(
-    <Button variant="primary" color="danger" disabled>
-      Primary Button
-    </Button>
-  );
-  expect(component.toJSON()).toMatchSnapshot();
+describe("Disabled Button", () => {
+  test("secondary disabled Button", () => {
+    const component = renderer.create(<Button disabled>Secondary</Button>);
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("primary disabled Button", () => {
+    const component = renderer.create(
+      <Button disabled variant="primary">
+        Primary
+      </Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+
+  test("tertiary disabled Button", () => {
+    const component = renderer.create(
+      <Button disabled variant="tertiary">
+        Primary
+      </Button>
+    );
+    expect(component.toJSON()).toMatchSnapshot();
+  });
 });
