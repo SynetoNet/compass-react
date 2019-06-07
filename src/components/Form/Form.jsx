2ac980a58d54376e3898b49b40d7783650cb3d1a
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-06-07 18:00:03 +0300

    refactor(Form): remove empty wrappers

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,26 +1,24 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+import React from "react";
+
 import BSForm from "react-bootstrap/Form";
 import CompassCheck from "./Check/Check.jsx";
 import CompassControl from "./Control/Control.jsx";
-import CompassGroup from "./Group/Group.jsx";
-import CompassLabel from "./Label/Label.jsx";
-import CompassRow from "./Row/Row.jsx";
 import CompassSwitch from "./Switch/Switch.jsx";
 
 import "./Form.scss";
+
 class Form extends React.Component {
-  static Group = CompassGroup;
-  static Label = CompassLabel;
+  static Group = BSForm.Group;
+  static Label = BSForm.Label;
   static Control = CompassControl;
   static Check = CompassCheck;
   static Text = BSForm.Text;
-  static Row = CompassRow;
+  static Row = BSForm.Row;
   static Switch = CompassSwitch;
 
   render() {
-    const { ...props } = this.props;
-    return <BSForm {...props} />;
+    return <BSForm {...this.props} />;
   }
 }
+
 export default Form;
--- src/components/Form/Group/Group.md
+++
@@ -1,9 +0,0 @@
-```jsx
-import { Form } from "../../../index";
-<Form>
-  <Form.Group controlId="exampleForm.ControlInput1">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="name@example.com" />
-  </Form.Group>
-</Form>;
-```
