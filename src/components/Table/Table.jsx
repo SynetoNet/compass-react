983f14060135c5aa7b1951efe27b07d4bfd3bf76
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-06-24 16:21:07 +0300

    refactor(Table): extract column options

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -8,6 +8,7 @@ import ToolkitProvider from "react-bootstrap-table2-toolkit";
 import Search from "./components/Search";
 import { getPaginationOptions } from "./components/paginationOptions";
 import { getSelectableOptions } from "./components/selectableOptions";
+import { getColumn } from "./components/columnOptions";
 
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 import "./Table.scss";
@@ -133,67 +134,4 @@ Table.propTypes = {
   onSelect: PropTypes.func
 };
 
-Table.defaultProps = {};
-
 export default Table;
-
-function capitalize(s) {
-  if (typeof s !== "string") return "";
-  return s.charAt(0).toUpperCase() + s.slice(1);
-}
-
-function getColumn(column) {
-  const {
-    field,
-    children,
-    header,
-    sort,
-    align,
-    renderCell,
-    width,
-    style,
-    headerStyle,
-    oneline,
-    ...otherProps
-  } = column;
-
-  const _style = {
-    ...style
-  };
-  const _headerStyle = {
-    ...headerStyle
-  };
-
-  if (oneline) {
-    Object.assign(_style, {
-      ..._style,
-      textOverflow: "ellipsis",
-      overflow: "hidden",
-      whiteSpace: "nowrap"
-    });
-  }
-
-  if (width) {
-    Object.assign(_style, {
-      width,
-      flex: "none"
-    });
-    Object.assign(_headerStyle, {
-      width,
-      flex: "none"
-    });
-  }
-
-  return {
-    dataField: field || "",
-    text: children || capitalize(header) || capitalize(field) || "",
-    sort: !!sort,
-    headerAlign: align,
-    align: align,
-    formatter: renderCell,
-    sortFunc: typeof sort === "function" ? sort : undefined,
-    style: { ..._style },
-    headerStyle: { ..._headerStyle },
-    ...otherProps
-  };
-}
