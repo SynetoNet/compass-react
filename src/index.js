6284c33abe680b2394008f1b8bce2ef0101634e7
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-06-26 16:51:18 +0300

    build: expose Table component for production build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as Dropdown } from "./components/Dropdown/Dropdown.jsx";
 // export { default as Form } from "./components/Form/Form.jsx";
 // export { default as Modal } from "./components/Modal/Modal.jsx";
 // export { default as Tabs } from "./components/Tabs/Tabs.jsx";
+export { default as Table } from "./components/Table/Table.jsx";
 
 export { Container } from "./layout/Container.jsx";
 export { Row } from "./layout/Row.jsx";
