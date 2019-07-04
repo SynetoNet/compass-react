bc0d5ab10f755f5eb9d1ed09c7057db5d5c405c6
Andrei Laza <alaza@opswat.com>
2019-07-04 18:07:15 +0300

    feat(Tooltip): add Tooltip component

---
+++ src/components/Tooltip/Tooltip.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "classnames";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import BSTooltip from "react-bootstrap/Tooltip";
+import "./Tooltip.scss";
+
+class Tooltip extends React.Component {
+  render() {
+    const { text, placement, trigger, ...props } = this.props;
+    console.log(placement);
+    return (
+      <OverlayTrigger
+        placement={placement}
+        show={"false"}
+        trigger={trigger}
+        overlay={<BSTooltip>{text}</BSTooltip>}
+      >
+        {this.props.children}
+      </OverlayTrigger>
+    );
+  }
+}
+
+Tooltip.propTypes = {
+  text: PropTypes.string,
+  placement: PropTypes.oneOf(["top", "bottom", "left", "right", "auto"]),
+  trigger: PropTypes.oneOf(["focus", "click", "hover"])
+};
+
+Tooltip.defaultProps = {
+  placement: "auto",
+  trigger: "hover"
+};
+
+export default Tooltip;
---
+++ src/components/Tooltip/Tooltip.md
@@ -0,0 +1,28 @@
+#### **Badge Example on hover**
+
+```jsx
+import Badge from "../Badge/Badge";
+
+<Tooltip placement="top" text="Some additional info" trigger="hover">
+  <Badge variant="strong">Over over me!</Badge>
+</Tooltip>;
+```
+
+#### **Button Example on click**
+
+```jsx
+import Button from "../Button/Button";
+
+<Tooltip placement="right" text="Some additional info" trigger="click">
+  <Button variant="primary">Click me!</Button>
+</Tooltip>;
+```
+
+#### **Button Example on focus**
+
+```jsx
+import Button from "../Button/Button";
+<Tooltip placement="left" text="Some additional info" trigger="focus">
+  <Button variant="primary">Click me!</Button>
+</Tooltip>;
+```
