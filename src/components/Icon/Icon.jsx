586054be97fa678009ebf5a835d246d1d5b37a84
denisdenes <denes.dennis@gmail.com>
2019-07-04 17:17:30 +0300

    refactor(Icon): Change Icon size implementation - remove the mapping object

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,17 +2,10 @@ import React      from 'react'
 import PropTypes  from "prop-types"
 import classNames from "classnames"
 
-const allSizes = {
-  xs: 'fa-xs',
-  sm: 'fa-sm',
-  md: 'fa-md',
-  lg: 'fa-lg'
-}
-
 const Icon = ({ name, color, size }) => {
   const classes = classNames({
     [ name ]: name,
-    [ allSizes[size] ]: allSizes[size]
+    [ `fa-${size}` ]: size
   })
 
   return (
