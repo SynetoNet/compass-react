e7a9fefa161cadf7b46650f3ae9e5f1adbb9703a
Andrei Laza <alaza@opswat.com>
2019-07-05 16:46:45 +0300

    test(Tooltip): add tests

---
+++ src/components/Tooltip/Tooltip.spec.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Tooltip from "./Tooltip";
+import Badge from "../Badge/Badge.jsx";
+import renderer from "react-test-renderer";
+
+import { configure, mount, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+
+configure({ adapter: new Adapter() });
+
+describe("Default Tooltip", () => {
+  const defaultExample = (
+    <Tooltip text="Some additional info">
+      <Badge variant="strong">Over over me!</Badge>
+    </Tooltip>
+  );
+
+  test("popover snapshop", () => {
+    const component = shallow(defaultExample);
+    expect(component).toMatchSnapshot();
+  });
+});
