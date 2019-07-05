70984024b2bc92cc6416f84f64faca3b97e2d33f
Andrei Pfeiffer <andrei@upsidedown.ro>
2019-07-05 13:36:14 +0300

    Code formatting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-export { default as Badge }    from "./components/Badge/Badge.jsx"
-export { default as Button }   from "./components/Button/Button.jsx"
-export { default as Dropdown } from "./components/Dropdown/Dropdown.jsx"
-export { default as Icon }     from "./components/Icon/Icon.jsx"
+export { default as Badge } from "./components/Badge/Badge.jsx";
+export { default as Button } from "./components/Button/Button.jsx";
+export { default as Dropdown } from "./components/Dropdown/Dropdown.jsx";
+export { default as Icon } from "./components/Icon/Icon.jsx";
 // export { default as DatePicker } from "./components/DatePicker/DatePicker.jsx";
 // export { default as Form } from "./components/Form/Form.jsx";
 // export { default as Modal } from "./components/Modal/Modal.jsx";
@@ -12,6 +12,6 @@ export { default as List } from "./components/List/List.jsx";
 export { default as Tooltip } from "./components/Tooltip/Tooltip.jsx";
 export { default as Popover } from "./components/Popover/Popover.jsx";
 
-export { Container } from "./layout/Container.jsx"
-export { Row }       from "./layout/Row.jsx"
-export { Col }       from "./layout/Col.jsx"
+export { Container } from "./layout/Container.jsx";
+export { Row } from "./layout/Row.jsx";
+export { Col } from "./layout/Col.jsx";
