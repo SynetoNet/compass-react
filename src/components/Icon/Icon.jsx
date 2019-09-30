93f8aff8659130fb7c583142fdd3853e4e08c564
Andrei Laza <alaza@opswat.com>
2019-09-30 17:28:17 +0300

    fix(Icon): pass props to children

--- src/components/Icon/Icon.jsx
+++ src/components/Icon/Icon.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 
-const Icon = ({ name, color, size, style, className = "" }) => {
+const Icon = ({ name, color, size, style, className = "", ...props }) => {
   const classes = classNames({
     [name]: name,
     [`fa-${size}`]: size,
     [className]: className
   });
 
-  return <i className={classes} style={{ color, ...style }} />;
+  return <i className={classes} style={{ color, ...style }} {...props}/>;
 };
 
 Icon.propTypes = {
